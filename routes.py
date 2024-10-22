bf8e78c03eb801adc6406d8ea8eadca3a376cdb3
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-22 12:43:28 +0200

    prøver at oprette post

--- routes.py
+++ routes.py
@@ -6,3 +6,18 @@ def register_routes(app, db):
     def index():
         elev = Elev.query.all()
         return str(elev)
+
+
+@app.route('/opret_elev', methods=['POST'])
+def opret_elev():
+    navn = request.form.get('navn')
+    fodselsdato = request.form.get('fodselsdato')
+
+    if not navn or not fodselsdato:
+        return "Fejl: Navn og fødselsdato skal udfyldes", 400
+
+    ny_elev = Elev(navn=navn, fodselsdato=fodselsdato)
+    db.session.add(ny_elev)
+    db.session.commit()
+
+    return "Elev oprettet!", 201
