5bf488ac657fae88b9e9c16a1999c4c7141f36db
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-22 12:01:08 +0200

    init

--- models.py
+++ models.py
@@ -2,8 +2,6 @@ from app import db
 from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()
-
 class Elev(db.Model):
     __tabelname__ = 'Elev'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,11 +10,3 @@ class Elev(db.Model):
 
     def __repr__(self):
         return f'Elev {self.navn} og {self.fodselsdato}'
-
-
-
-class Lektion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    navn = db.Column(db.String(100), nullable=False)
-    elev_id = db.Column(db.Integer, db.ForeignKey('elev.id'), nullable=False)
-    elev = db.relationship('Elev', backref=db.backref('lektioner', lazy=True))
