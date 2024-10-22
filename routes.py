1352aeb61c8dd88780f097aa93745428b852db62
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-22 14:11:37 +0200

    prøver opret elev

--- routes.py
+++ routes.py
@@ -2,22 +2,21 @@ from flask import render_template, request
 from models import Elev
 
 def register_routes(app, db):
-    @app.route('/')
+    @app.route('/', )
     def index():
         elev = Elev.query.all()
         return str(elev)
 
 
-@app.route('/opret_elev', methods=['POST'])
-def opret_elev():
-    navn = request.form.get('navn')
-    fodselsdato = request.form.get('fodselsdato')
+    @app.route('/opret_elev', methods=["POST"])
+    def opret_elev():
+        navn = request.form.get('navn')
+        fodselsdato = request.form.get('fodselsdato')
+        if not navn or not fodselsdato:
+            return "Fejl: Navn og fødselsdato skal udfyldes", 400
 
-    if not navn or not fodselsdato:
-        return "Fejl: Navn og fødselsdato skal udfyldes", 400
-
-    ny_elev = Elev(navn=navn, fodselsdato=fodselsdato)
-    db.session.add(ny_elev)
-    db.session.commit()
+        ny_elev = Elev(navn=navn, fodselsdato=fodselsdato)
+        db.session.add(ny_elev)
+        db.session.commit()
 
     return "Elev oprettet!", 201
