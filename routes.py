aba09e46f33f6dac8de636f72dcdcc22e8ef9d55
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-10-23 13:44:30 +0200

    postman test POST og GET!

--- routes.py
+++ routes.py
@@ -20,8 +20,15 @@ def register_routes(app, db):
             db.session.add(ny_elev)
             db.session.commit()
             return redirect(url_for('index'))
-
         return render_template('index.html')
+
+    @app.route('/elever', methods=["GET"])
+    def get_elever():
+        elever = Elev.query.all()
+        elev_liste = [{"navn": elev.navn, "fodselsdato": elev.fodselsdato} for elev in elever]
+        return {"elever": elev_liste}
+
+
 
 
 
