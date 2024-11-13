72633312dbed9cf46ab569d3ff15c8dcab585380
Lyanne Fernandez <lbfe44184@edu.ucl.dk>
2024-11-13 14:57:08 +0100

    ændringer i db

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ db = SQLAlchemy()
 # Many-to-Many tabel til at forbinde elever og danselektioner
 attendance = db.Table('attendance',
     db.Column('elev_id', db.Integer, db.ForeignKey('elev.id')),
-    db.Column('danselektion_id', db.Integer, db.ForeignKey('danselektion.id'))
+    # db.Column('danselektion_id', db.Integer, db.ForeignKey('danselektion.id'))
 )
 
 
@@ -28,62 +28,68 @@ class Elev(db.Model):
 class Lokation(db.Model):
     __tablename__ = 'lokation'
     id = db.Column(db.Integer, primary_key=True)
-    navn = db.Column(db.String(100), nullable=False) #fx stenløse el. hvidovre
+    adresse = db.Column(db.String(100), nullable=False) #fx stenløse el. hvidovre
     #optional: adresse eller andet relevant info
+
+class Stilart(db.Model):
+    __tablename__ = 'stilart'
+    id = db.Column(db.Integer, primary_key=True)
+    stilart = db.Column(db.String(100), nullable=False)
+    beskrivelse = db.Column(db.String(100), nullable=False)
 
 
 class Instruktor(db.Model):
     __tablename__ = 'instruktor'
     id = db.Column(db.Integer, primary_key=True)
-    navn = db.Column(db.String(100), nullable=False)
+    fornavn = db.Column(db.String(100), nullable=False)
+    efternavn = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     telefon = db.Column(db.String(100))
 
     def __str__(self):
-        return f'Instruktor {self.navn}'
+        return f'Instruktor {self.fornavn}'
 
 class Dansehold(db.Model):
     __tablename__ = 'dansehold'
     id = db.Column(db.Integer, primary_key=True)
     stilart = db.Column(db.String(50), nullable=False) #fx Disco, Showdance
     instruktor = db.Column(db.String(100))
-    beskrivelse = db.Column(db.String(200))
     lokation_id = db.Column(db.Integer, db.ForeignKey('lokation.id'))
     lokation = db.relationship('Lokation')
 
     def __repr__(self):
         return f'Dansehold {self.stilart}'
 
-class Danselektion(db.Model):
-    __tablename__ = 'danselektion'
-    id = db.Column(db.Integer, primary_key=True)
-    dato = db.Column(db.String, nullable=False) #fx en given dato
-    tidspunkt = db.Column(db.Time, nullable=False) # tidspunkt for danselektion
-    dansehold_id = db.Column(db.Integer, db.ForeignKey('dansehold.id')) #tilknytning til danselektioner
-    dansehold = db.relationship('Dansehold')
-    elev_id = db.Column(db.Integer, db.ForeignKey('elev.id'))
-    elev = db.relationship('Elev')
-    lokation_id = db.Column(db.Integer, db.ForeignKey('lokation.id')) #tilknytning til lokation
-    lokation = db.relationship('Lokation')
-    instruktor_id = db.Column(db.Integer, db.ForeignKey('instruktor.id'))  # Instruktør
-    instruktor = db.relationship('Instruktor')  # Instruktør for lektionen
-    attendance = db.relationship('Elev', secondary='attendance', backref='danselektioner') #Fremmøde for hver elev
-
-    @property
-    def iso_dato(self):
-        # Check type og værdi af dato
-        print(f"Type of dato: {type(self.dato)}, Value: {self.dato}")
-
-        if isinstance(self.dato, str):
-            try:
-                # Konverterer datoen fra string til ISO format
-                return datetime.fromisoformat(self.dato).isoformat()
-            except ValueError:
-                print("Datoen er ikke i gyldigt ISO-format.")
-                return None
-        else:
-            print("Dato er ikke en streng.")
-            return None
+# class Danselektion(db.Model):
+#     __tablename__ = 'danselektion'
+#     id = db.Column(db.Integer, primary_key=True)
+#     dato = db.Column(db.String, nullable=False) #fx en given dato
+#     tidspunkt = db.Column(db.Time, nullable=False) # tidspunkt for danselektion
+#     dansehold_id = db.Column(db.Integer, db.ForeignKey('dansehold.id')) #tilknytning til danselektioner
+#     dansehold = db.relationship('Dansehold')
+#     elev_id = db.Column(db.Integer, db.ForeignKey('elev.id'))
+#     elev = db.relationship('Elev')
+#     lokation_id = db.Column(db.Integer, db.ForeignKey('lokation.id')) #tilknytning til lokation
+#     lokation = db.relationship('Lokation')
+#     instruktor_id = db.Column(db.Integer, db.ForeignKey('instruktor.id'))  # Instruktør
+#     instruktor = db.relationship('Instruktor')  # Instruktør for lektionen
+#     attendance = db.relationship('Elev', secondary='attendance', backref='danselektioner') #Fremmøde for hver elev
+#
+#     @property
+#     def iso_dato(self):
+#         # Check type og værdi af dato
+#         print(f"Type of dato: {type(self.dato)}, Value: {self.dato}")
+#
+#         if isinstance(self.dato, str):
+#             try:
+#                 # Konverterer datoen fra string til ISO format
+#                 return datetime.fromisoformat(self.dato).isoformat()
+#             except ValueError:
+#                 print("Datoen er ikke i gyldigt ISO-format.")
+#                 return None
+#         else:
+#             print("Dato er ikke en streng.")
+#             return None
 
 
 # class prøvetime(db.Model):
